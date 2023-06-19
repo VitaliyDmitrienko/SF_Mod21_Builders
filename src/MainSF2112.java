3504335616902f653e1b25a9dd1095564cb74b12
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-06-19 19:10:49 +0300

    1st.

---
+++ src/MainSF2112.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MainSF2112 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!!!");
+        System.out.println(StringUtils.upperCase("Hello world!!!"));
+    }
+}
